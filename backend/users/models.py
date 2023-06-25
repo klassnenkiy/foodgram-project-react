5f8474eaace91c2020431643de7d2a8d2986e2d2
klassnenkiy <klassnenkiy@gmail.com>
2023-06-25 23:04:50 +0300

    users

--- backend/users/models.py
+++ backend/users/models.py
@@ -4,23 +4,57 @@ from django.db.models import UniqueConstraint
 
 
 class User(AbstractUser):
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = [
+        'username',
+        'first_name',
+        'last_name',
+    ]
+    username = models.CharField(
+        'Логин',
+        max_length=150,
+        unique=True,
+    )
+    first_name = models.CharField(
+        'Имя',
+        max_length=150,
+        blank=False
+    )
+    last_name = models.CharField(
+        'Фамилия',
+        max_length=150,
+        blank=False
+    )
+    password = models.CharField(
+        'Пароль',
+        max_length=150
+    )
+    email = models.EmailField(
+        'email address',
+        max_length=254,
+        unique=True,
+    )
 
     class Meta:
+        ordering = ('id',)
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
 
 
 class Subscribe(models.Model):
     user = models.ForeignKey(
         User,
         related_name='subscriber',
-        verbose_name="Подписчик",
+        verbose_name='Подписчик',
         on_delete=models.CASCADE,
     )
     author = models.ForeignKey(
         User,
         related_name='subscribing',
-        verbose_name="Автор",
+        verbose_name='Автор',
         on_delete=models.CASCADE,
     )
 
