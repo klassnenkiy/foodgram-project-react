8610c8a33a6e40f565d345a0b20009710e148b37
klassnenkiy <klassnenkiy@gmail.com>
2023-06-29 17:26:36 +0300

    infra

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -103,11 +103,11 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static_backend/'
-STATIC_ROOT = BASE_DIR / 'static_backend/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
