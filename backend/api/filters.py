3f2c0dce66661e33e9c29ac08690b095bba1e618
klassnenkiy <klassnenkiy@gmail.com>
2023-07-01 04:24:58 +0300

    1st

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,6 +1,7 @@
 from django_filters.rest_framework import FilterSet, filters
-from recipes.models import Favorite, Recipe, ShoppingCart
 from rest_framework.filters import SearchFilter
+
+from recipes.models import Favorite, Recipe, ShoppingCart
 
 
 class RecipeFilter(FilterSet):
