a84bce6e6214f46be2283f2d4049079b0e550a3c
klassnenkiy <klassnenkiy@gmail.com>
2023-07-01 23:26:00 +0300

    final

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -149,7 +149,7 @@ class RecipeToRepresentationSerializer(serializers.ModelSerializer):
 
 class RecipeSerializer(serializers.ModelSerializer):
     author = UserSerializer(read_only=True)
-    tags = TagSerializer(read_only=True, many=True)
+    tags = TagSerializer(many=True)
     ingredients = IngredientInRecipeSerializer(many=True)
     is_favorited = serializers.SerializerMethodField(read_only=True)
     is_in_shopping_cart = serializers.SerializerMethodField(read_only=True)
@@ -178,9 +178,15 @@ class RecipeSerializer(serializers.ModelSerializer):
             recipe=obj, cart_owner=request.user).exists()
 
     def validate(self, data):
-        tags = self.initial_data.get('tags')
-        ingredients = self.initial_data.get('ingredients')
+        tags = data.get('tags')
+        ingredients = data.get('ingredients')
         cooking_time = data.get('cooking_time')
+        if not tags:
+            raise serializers.ValidationError({
+                'tags': 'Кажется вы забыли указать тэги'})
+        if not ingredients:
+            raise serializers.ValidationError({
+                'ingredients': 'Кажется вы забыли указать ингредиенты'})
         validate_tags(tags, Tag)
         validate_ingredients(ingredients, Ingredient)
         validate_cooking_time(cooking_time)
